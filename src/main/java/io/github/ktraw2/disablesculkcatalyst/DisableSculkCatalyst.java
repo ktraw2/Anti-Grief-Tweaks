7c7457b8a782c4b85fe4b3e64162b611d7cea1c3
Kevin Traw <ktraw2@gmail.com>
2023-01-26 17:13:59 -0600

    Make the disabling of the sculk catalyst configurable by a gamerule

--- src/main/java/io/github/ktraw2/disablesculkcatalyst/DisableSculkCatalyst.java
+++ src/main/java/io/github/ktraw2/disablesculkcatalyst/DisableSculkCatalyst.java
@@ -1,21 +1,17 @@
 package io.github.ktraw2.disablesculkcatalyst;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DisableSculkCatalyst implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("disable-sculk-catalyst");
+	public static final GameRules.Key<GameRules.BooleanRule> DISABLE_SCULK_CATALYST =
+			GameRuleRegistry.register("disableSculkCatalyst", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(false));
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+	public void onInitialize() {}
 }
--- src/main/java/io/github/ktraw2/disablesculkcatalyst/mixin/DisableSculkCatalystSpreadMixin.java
+++ src/main/java/io/github/ktraw2/disablesculkcatalyst/mixin/DisableSculkCatalystSpreadMixin.java
@@ -1,9 +1,11 @@
 package io.github.ktraw2.disablesculkcatalyst.mixin;
 
+import io.github.ktraw2.disablesculkcatalyst.DisableSculkCatalyst;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SculkCatalystBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.SculkCatalystBlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -13,8 +15,11 @@ import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.event.listener.GameEventListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = "net.minecraft.block.entity.SculkCatalystBlockEntity")
+@Mixin(SculkCatalystBlockEntity.class)
 public abstract class DisableSculkCatalystSpreadMixin extends BlockEntity implements GameEventListener {
 	public DisableSculkCatalystSpreadMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
 		super(type, pos, state);
@@ -23,7 +28,22 @@ public abstract class DisableSculkCatalystSpreadMixin extends BlockEntity implem
 	@Shadow
 	abstract void triggerCriteria(LivingEntity deadEntity);
 
-	public boolean listen(ServerWorld world, GameEvent event, GameEvent.Emitter emitter, Vec3d emitterPos) {
+	@Inject(
+			method = "listen(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/world/event/GameEvent;Lnet/minecraft/world/event/GameEvent$Emitter;Lnet/minecraft/util/math/Vec3d;)Z",
+			at = @At("HEAD"),
+			cancellable = true
+	)
+	public void injected(
+			final ServerWorld world,
+			final GameEvent event,
+			final GameEvent.Emitter emitter,
+			final Vec3d emitterPos,
+			final CallbackInfoReturnable<Boolean> callbackInfo
+	) {
+		if (!world.getGameRules().getBoolean(DisableSculkCatalyst.DISABLE_SCULK_CATALYST)) {
+			return;
+		}
+
 		final Entity entity = emitter.sourceEntity();
 		if (event == GameEvent.ENTITY_DIE && entity instanceof LivingEntity) {
 			LivingEntity livingEntity = (LivingEntity)entity;
@@ -34,8 +54,9 @@ public abstract class DisableSculkCatalystSpreadMixin extends BlockEntity implem
 				livingEntity.disableExperienceDropping();
 				SculkCatalystBlock.bloom(world, this.pos, this.getCachedState(), world.getRandom());
 			}
-			return true;
+			callbackInfo.setReturnValue(true);
 		}
-		return false;
+
+		callbackInfo.setReturnValue(false);
 	}
 }
